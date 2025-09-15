23b80e3596729795eabd5dd026c59a229ed8d7d1
Nelson Fleig <nelson.fleig@rover.com>
2025-09-16 00:50:33 +0200

    fix install issues

--- run_after_source_zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-if [ -n "${CODESPACES}" ]; then
-  # change default shell to zsh
-  sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
-fi
-
-source ~/.zshrc
--- setup.sh
+++ setup.sh
@@ -3,14 +3,21 @@
 # exit on error and print each command
 set -euxo pipefail
 
+# ~/.dotfiles ==> directory where this script is located
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+[[ -e ~/.dotfiles ]] || ln -s "$DIR" ~/.dotfiles
+
 # Install chezmoi
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
 
 # Set default shell to zsh
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
-# Install OMZ
-ZSH=sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Reset and install OMZ
+if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
+    rm -rf $HOME/.oh-my-zsh
+    ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
 # Install Powerlevel10K
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
